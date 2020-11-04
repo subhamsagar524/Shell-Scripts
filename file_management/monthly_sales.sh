8b011e4c0b18a210dc8ffc8d0703d98a7f74bd68
subhamsagar524 <my.subham524@gmail.com>
2020-11-04 13:25:07 +0530

    modified

--- file_management/monthly_sales.sh
+++ file_management/monthly_sales.sh
@@ -17,26 +17,33 @@ getHighest()
 	read -p "Enter the month-number(MM) to get the highest sale: " m
 
 	case $m in
-		1 ) cat monthly_sales_data.txt | awk -F " " '{print $1}' > test	  ;;
-		2 ) cat monthly_sales_data.txt | awk -F " " '{print $2}' > test	  ;;
-		3 ) cat monthly_sales_data.txt | awk -F " " '{print $3}' > test	  ;;
-		4 ) cat monthly_sales_data.txt | awk -F " " '{print $4}' > test	  ;;
-		5 ) cat monthly_sales_data.txt | awk -F " " '{print $5}' > test	  ;;
-		6 ) cat monthly_sales_data.txt | awk -F " " '{print $6}' > test	  ;;
-		7 ) cat monthly_sales_data.txt | awk -F " " '{print $7}' > test	  ;;
-		8 ) cat monthly_sales_data.txt | awk -F " " '{print $8}' > test	  ;;
-		9 ) cat monthly_sales_data.txt | awk -F " " '{print $9}' > test	  ;;
-		10 ) cat monthly_sales_data.txt | awk -F " " '{print $10}' > test ;;
-		11 ) cat monthly_sales_data.txt | awk -F " " '{print $11}' > test ;;
-		12 ) cat monthly_sales_data.txt | awk -F " " '{print $12}' > test ;;
+		1 ) cat monthly_sales_data.txt | awk -F " " '{print $1}' | sort>test	  ;;
+		2 ) cat monthly_sales_data.txt | awk -F " " '{print $2}' | sort>test	  ;;
+		3 ) cat monthly_sales_data.txt | awk -F " " '{print $3}' | sort>test	  ;;
+		4 ) cat monthly_sales_data.txt | awk -F " " '{print $4}' | sort>test	  ;;
+		5 ) cat monthly_sales_data.txt | awk -F " " '{print $5}' | sort>test	  ;;
+		6 ) cat monthly_sales_data.txt | awk -F " " '{print $6}' | sort>test	  ;;
+		7 ) cat monthly_sales_data.txt | awk -F " " '{print $7}' | sort>test	  ;;
+		8 ) cat monthly_sales_data.txt | awk -F " " '{print $8}' | sort>test	  ;;
+		9 ) cat monthly_sales_data.txt | awk -F " " '{print $9}' | sort>test	  ;;
+		10 ) cat monthly_sales_data.txt | awk -F " " '{print $10}' | sort>test ;;
+		11 ) cat monthly_sales_data.txt | awk -F " " '{print $11}' | sort>test ;;
+		12 ) cat monthly_sales_data.txt | awk -F " " '{print $12}' | sort>test ;;
 		* ) echo "Invalid Month..."; exit 0	;;
 	esac
-	
-	data=`cat test | tr " " "\n" | sort -nr`
-	rm test
 
-	echo -e "\nSale for the month $m..."
-	echo $data
+	i=0
+	while read x
+	do
+		a[$i]=$x
+		((i++))
+	done < test
+	rm test
+	
+	l=`expr ${#a[*]} - 1`
+	echo "Sales for month $m..."
+	echo Min sale=${a[0]}
+	echo Max sale=${a[$l]}
 }
 
 echo "Sales Records..."
