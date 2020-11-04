c2f4a164c969bc36ad1f11300c8f266c127596ae
subhamsagar524 <my.subham524@gmail.com>
2020-11-04 10:52:06 +0530

    Employees management files

--- file_management/store_employee_records.sh
+++ file_management/store_employee_records.sh
@@ -2,6 +2,7 @@
 
 addEmployee()
 {
+	echo ""
 	read -p "Name: " ename
 	read -p "Employee ID: " eid
 	read -p "Address: " addr
@@ -15,14 +16,46 @@ addEmployee()
 
 displayEmployee()
 {
+	echo ""
 	read -p "Enter eid to display: " eid
-	awk '/${eid}/ {print}' employees.txt 
+	grep -w $eid employees.txt > temp
+
+	if [ -s temp ]
+	then
+		echo -e "Employee Details..."
+		details=`cat temp | awk -F " " '{print $1}'`
+		echo "Name          : $details"
+		details=`cat temp | awk -F " " '{print $2}'`
+		echo "Employee ID   : $details"
+		details=`cat temp | awk -F " " '{print $3}'`
+		echo "Address       : $details"
+		details=`cat temp | awk -F " " '{print $4}'`
+		echo "Mob No        : $details"
+		details=`cat temp | awk -F " " '{print $5}'`
+		echo "Joining Year  : $details"
+		details=`cat temp | awk -F " " '{print $6}'`
+		echo "Current Salary: $details"
+	else
+		echo -e "\nEmployee with EID $eid doesn't exists..."
+	fi
+
+	rm temp
+}
+
+deleteEmployee()
+{
+	echo ""
+	read -p "Enter eid to delete: " eid
+	grep -v $eid employees.txt > temp
+	cat temp > employees.txt
+
+	rm temp
 }
 
 echo "Storing of details of Employees..."
 while [ true ]
 do
-	echo "1. Add record of Employees"
+	echo -e "\n1. Add record of Employees"
 	echo "2. Display record of Employees"
 	echo "3. Delete record of Employees"
 	echo "4. Exit"
