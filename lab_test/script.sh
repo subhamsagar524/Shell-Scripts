83cde084e945b733bd3396e16c35dfd1b168a952
Subham Sagar Paira <42293958+subhamsagar524@users.noreply.github.com>
2021-01-20 20:52:11 +0530

    Add files via upload

--- lab_test/script.sh
+++ lab_test/script.sh
@@ -1,7 +1,7 @@
 : '
-	11) Write a bash script to input n elements 
-	    in the range 100-1000000,
-	    then count how many armstrong numbers are present,then display
+    11) Write a bash script to input n elements 
+            in the range 100-1000000,
+            then count how many armstrong numbers are present,then display
 '
 
 #!/bin/bash
@@ -9,29 +9,29 @@
 # Function to check if the number is armstrong or not
 function isArmstrong
 {
-	count=0
-	declare -i i
-	for ((i = 100; i <= n; i++))
-	do
-		sum=0
-		item=$i
-		l=`expr length $i`
-		while [ $item -ne 0 ]
-		do
-			rem=`expr $item % 10`
-			pow=`echo "$rem^$l" | bc`
-			sum=`expr $sum + $pow`
-			item=`expr $item / 10`
-		done
+    count=0
+    declare -i i
+    for ((i = 100; i <= n; i++))
+    do
+        sum=0
+        item=$i
+        l=`expr length $i`
+        while [ $item -ne 0 ]
+        do
+            rem=`expr $item % 10`
+            pow=`echo "$rem^$l" | bc`
+            sum=`expr $sum + $pow`
+            item=`expr $item / 10`
+        done
 
-		if [ $sum -eq $i ]
-		then
-			echo -e "$i, "
-			count=`expr $count + 1`
-		fi
-	done
+        if [ $sum -eq $i ]
+        then
+            echo -e "$i, "
+            count=`expr $count + 1`
+        fi
+    done
 
-	echo -e "\nTotal Armstrong numbers: $count"
+    echo -e "\nTotal Armstrong numbers: $count"
 }
 
 # Driver code
